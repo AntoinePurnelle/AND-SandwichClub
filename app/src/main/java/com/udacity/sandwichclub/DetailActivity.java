ed3385cb85f9d7bc645daaf93991f8e23f96f0e2
Antoine Purnelle <antoine.purnelle@ouftech.net>
2018-02-23 18:09:50 +0100

    Display sandwich data + UI adjustments

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,11 +2,13 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -71,7 +75,6 @@ public class DetailActivity extends AppCompatActivity {
         // Collapsing toolbar layout inspired from http://www.tutos-android.com/design-support-library-collapsingtoolbarlayout
         setSupportActionBar(toolbar);
         ViewCompat.setTransitionName(findViewById(R.id.appBarLayout), "Name");
-        collapsingToolbar.setTitle("My Sandwich");
         collapsingToolbar.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
@@ -83,12 +86,13 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(imageIV);
 
         setTitle(sandwich.getMainName());
+        collapsingToolbar.setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -96,7 +100,40 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        if (sandwich.getAlsoKnownAs() == null || sandwich.getAlsoKnownAs().isEmpty()) {
+            akaLabelTv.setVisibility(View.GONE);
+            akaTv.setVisibility(View.GONE);
+        } else {
+            akaTv.setText(toBulletPoints(sandwich.getAlsoKnownAs()));
+        }
 
+        if (TextUtils.isEmpty(sandwich.getPlaceOfOrigin())) {
+            originLabelTv.setVisibility(View.GONE);
+            originTv.setVisibility(View.GONE);
+        } else {
+            originTv.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        if (TextUtils.isEmpty(sandwich.getDescription())) {
+            descriptionLabelTv.setVisibility(View.GONE);
+            descriptionTv.setVisibility(View.GONE);
+        } else {
+            descriptionTv.setText(sandwich.getDescription());
+        }
+
+        if (sandwich.getIngredients() == null || sandwich.getIngredients().isEmpty()) {
+            ingredientsLabelTv.setVisibility(View.GONE);
+            ingredientsLabelTv.setVisibility(View.GONE);
+        } else {
+            ingredientsTv.setText(toBulletPoints(sandwich.getIngredients()));
+        }
+    }
+
+    private String toBulletPoints(@NonNull List<String> list) {
+        if (list.size() == 1)
+            return list.get(0);
+
+        return "\u2022 " + TextUtils.join("\n\u2022 ", list);
     }
 }
